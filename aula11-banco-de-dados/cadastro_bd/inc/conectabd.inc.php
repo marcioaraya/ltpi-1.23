150f0f23818c92408de0f05b327116a41a88c3e1
Marcio Eduardo Araya <marcio.araya@edu.senac.br>
2023-06-20 12:01:19 -0300

    acrescente comentários sobre conexão com mysql

--- aula11-banco-de-dados/cadastro_bd/inc/conectabd.inc.php
+++ aula11-banco-de-dados/cadastro_bd/inc/conectabd.inc.php
@@ -1,5 +1,10 @@
   <?php
   // conectabd.inc.php
+  // 1) se usar o mysql do XAMPP no computador da Faculdade
+  // altere localhost mysqli_connect("localhost:3307", "senac", "", "curso")
+  // 2) se usar o mysql no Workbench, use como abaixo
+  // será necessário executar o script que está no arquivo "criar-usuario-da-aplicacao.sql"
+  // que está na pasta scripts_banco
   $link = mysqli_connect("localhost", "aplicacao_curso", "curso123", "curso");
   
   // mysqli_connect_errno - devolve o código do erro
